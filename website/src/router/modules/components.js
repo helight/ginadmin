a800235b4bc1e356c147a5eeaf2e2adad9fcba78
helight <helight@zhwen.org>
2019-07-11 20:26:59 +0800

    delete mk

--- website/src/router/modules/components.js
+++ website/src/router/modules/components.js
@@ -19,12 +19,6 @@ const componentsRouter = {
       meta: { title: 'tinymce' }
     },
     {
-      path: 'markdown',
-      component: () => import('@/views/components-demo/markdown'),
-      name: 'MarkdownDemo',
-      meta: { title: 'markdown' }
-    },
-    {
       path: 'json-editor',
       component: () => import('@/views/components-demo/jsonEditor'),
       name: 'JsonEditorDemo',
